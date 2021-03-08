6278be981422faacea4e2e13e3350b06bb80b74a
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2021-03-08 17:19:03 -0300

    Update

---
+++ ubuntu/ubuntu.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Ubuntu 20.04 tested.
+
+echo
+echo "System update"
+echo
+apt update && apt upgrade && apt autoremove
+echo
+echo "Installing VIM"
+echo
+apt install vim
+echo
+echo "Installing Steam"
+echo
+apt install steam
+echo
+echo "Installing neofetch"
+echo
+apt install neofetch
+echo
+echo "Installing gimp"
+echo
+apt install gimp
+echo
+echo "Installing Gnome Disks"
+echo
+apt install gnome-disk-utility
+echo
+echo "Installing OBS Studio"
+echo
+apt install obs-studio
+echo
+echo "Installing Unzip"
+echo
+apt install unzip
+echo
+echo "Installing VLC"
+echo
+apt install vlc
+echo

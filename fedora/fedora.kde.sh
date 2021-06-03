91a1ae20fa877823a485b4a27d1deed36afb69eb
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2021-06-03 11:56:50 -0300

    Update

--- fedora/fedora.kde.sh
+++ fedora/fedora.kde.sh
@@ -17,56 +17,26 @@ echo "Installing codecs"
 echo
 dnf install binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel dkms qt5-qtx11extras libxkbcommon
 echo
-echo "Installing Spotify 1/1"
-echo
-dnf install lpf-spotify-client
-echo
-echo "Installing Spotify 2/2"
-echo
-lpf update
-echo
 echo "Installing Steam"
 echo
-dnf install steam -y
-echo
+dnf install steam
 echo
 echo " Installing neofetch"
 dnf install neofetch
-echo
-echo
-dnf install neofetch
-echo
-echo
-echo "Installing Libreoffice"
-echo
-dnf install libreoffice
-echo
-echo "Installing K3B"
-echo
-dnf install k3b
 echo
 echo "Installing Unzip"
 echo
 dnf install unzip
 echo
-echo "Installing Gparted"
-echo
-dnf install gparted
-echo
 echo "Installing HTOP command"
 echo
 dnf install htop
-echo
-echo "Installing Brasero"
-echo
-dnf install brasero
 echo
 echo "Installing Kdenlive"
 echo
 dnf install kdenlive
 echo
 echo "Installing VLC"
-echo
 echo
 dnf install vlc
 echo
@@ -78,25 +48,13 @@ echo "Installing OBS Studio"
 echo
 dnf install obs-studio
 echo
+echo "Installing Clip Grap"
+echo
+dnf install clipgrab
+echo
 echo "Installing Discord"
 echo
 dnf install discord
-echo
-echo "Installing SAMBA"
-echo
-dnf install samba
-echo
-echo "Installing Sublime Text 1/3"
-echo
-rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-echo
-echo "Installing Sublime Text 2/3"
-echo
-dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-echo
-echo "Installing Sublime Text 3/3"
-echo
-dnf install sublime-text
 echo
 echo "Installing Visual Studio Code 1/4"
 echo
@@ -113,10 +71,6 @@ echo
 echo "Installing Visual Studio Code 4/4"
 echo
 dnf install code
-echo
-echo "Installing Clip Grap"
-echo
-dnf install clipgrab
 echo
 echo "Your system has been updated and your programs have been successfully installed."
 echo
